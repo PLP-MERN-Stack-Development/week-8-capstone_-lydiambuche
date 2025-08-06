fd14bdc8f4f42ffeb2b40128eeeb0924a93ac8ca
lydiambuche <lydiambuche@gmail.com>
2025-08-06 14:23:36 +0300

    Initial commit for Week 8 Capstone - Quicklog

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { assets, footer_data } from "../assets/assets.js";
+
+const Footer = () => {
+  return (
+    <div className="px-6 md:px-16 lg:px-24 xl:px-32 bg-primary/3">
+      <div className="flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-gray">
+
+      <div>
+        <img src={assets.logo} alt="logo" className="w-32 sm:w-44"/>
+        <p className="max-w-[410px] mt-6 text-gray-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, delectus excepturi. Est ullam earum, nam laudantium sequi rerum expedita cum molestiae aspernatur ipsum fugiat doloremque nostrum non quae maiores nobis?</p>
+      </div>
+
+      <div className="flex flex-wrap justify-between w-full md:w-[45%] gap-5">
+        {footer_data.map((section, index) => (
+          <div key={index}>
+            <h3 className="font-semibold text-base text-gray-900 md:md-5 mb-4">{section.title}</h3>
+            <ul className="text-sm space-y-1">
+              {section.links.map((link, i) => (
+                <li key={i}>
+                  <a href="#" className="hover:underline transition text-gray-500">{link}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+
+      </div>
+      <p className="py-4 text-center text-sm md:text-base text-gray-500/80">copyright 2025 © QuickBlog - Sam All Right Reserved</p>
+    </div>
+  );
+};
+
+export default Footer;
